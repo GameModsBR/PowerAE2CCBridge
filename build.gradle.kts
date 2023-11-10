5c0d50360d362ab4fb109f9aef81867c306e2e5f
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2023-11-10 01:51:11 -0300

    (feat): bump dependencies to 1.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 /*
  * Copyright (c) 2022 Leon Linhart
  *
@@ -19,23 +21,13 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import com.github.themrmilchmann.build.*
-import com.github.themrmilchmann.build.BuildType
-import io.github.themrmilchmann.gradle.publish.curseforge.*
 
 plugins {
     java
-    alias(libs.plugins.curseforge.publish)
     alias(libs.plugins.loom)
 }
 
-version = "1.0.0-1.18.2-FABRIC-0.2"
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(19))
-    }
-}
+version = "1.0.0-1.20.1-FABRIC"
 
 tasks {
     compileJava {
@@ -49,45 +41,6 @@ tasks {
             expand("version" to "${project.version}")
         }
     }
-}
-
-publishing {
-    repositories {
-        curseForge {
-            apiKey.set(deployment.cfApiKey)
-        }
-    }
-    publications {
-        create<CurseForgePublication>("curseForge") {
-            projectID.set(715346) // https://www.curseforge.com/minecraft/mc-mods/ae2cc-bridge
-
-            artifact(tasks.remapJar) {
-                changelog = changelog()
-                displayName = "AE2CC Bridge ${project.version}"
-                releaseType = ReleaseType.RELEASE
-            }
-        }
-    }
-}
-
-fun changelog(): Changelog {
-    if (deployment.type == BuildType.SNAPSHOT) return Changelog("", ChangelogType.TEXT)
-
-    val mc = project.version.toString() // E.g. 1.0.0-1.16.5-FABRIC-1.0
-        .substringAfter('-')            //            1.16.5-FABRIC-1.0
-        .substringBefore('-')           //            1.16.5-FABRIC
-        .substringBefore('-')           //            1.16.5
-        .let {
-            if (it.count { it == '.' } == 1)
-                it
-            else
-                it.substringBeforeLast('.')
-        }                               //            1.16
-
-    return Changelog(
-        content = File(rootDir, "docs/changelog/$mc/${project.version}.md").readText(),
-        type = ChangelogType.MARKDOWN
-    )
 }
 
 repositories {
@@ -126,20 +79,36 @@ repositories {
         }
     }
 
-    mavenLocal {
+    maven(url = "https://squiddev.cc/maven/") {
         content {
-            includeGroup("dan200.computercraft")
+            includeGroup("cc.tweaked")
+            includeModule("org.squiddev", "Cobalt")
+        }
+    }
+
+    exclusiveContent {
+        forRepository {
+            maven {
+                name = "Modrinth"
+                url = URI("https://api.modrinth.com/maven")
+            }
+        }
+        filter {
+            includeGroup("maven.modrinth")
         }
     }
 }
 
 dependencies {
     minecraft(libs.minecraft)
-    mappings(loom.officialMojangMappings())
+    mappings("net.fabricmc:yarn:1.20.1+build.10")
 
     modImplementation(libs.fabric.api)
     modImplementation(libs.fabric.loader)
 
     modImplementation(libs.ae2)
-    modImplementation(libs.cc.restitched)
+    modRuntimeOnly(libs.ae2.runtime)
+
+    modImplementation(libs.cc.tweaked)
+    modRuntimeOnly(libs.cc.tweaked.runtime)
 }
